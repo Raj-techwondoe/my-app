9c838d2fecba9bac8f60bc824e0873ed5633bb3f
Raj-techwondoe <raj.upadhyay@techwondoe.com>
2024-01-26 15:44:37 +0530

    Name changed to Raju

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Noshu</code> and save to reload.
+          Edit <code>Raju</code> and save to reload.
         </p>
         <a
           className="App-link"
